c7b0b77edef66dfa80de44bc25084f549fd2feaa
AvailableUsernameA <huc014@ucsd.edu>
2025-11-20 18:20:17 -0500

    asr

--- IDL-HW4/hw4lib/data/asr_dataset.py
+++ IDL-HW4/hw4lib/data/asr_dataset.py
@@ -91,7 +91,7 @@ class ASRDataset(Dataset):
         self.fbank_dir   = os.path.join(self.config['root'], self.partition, 'fbank')
         
         # TODO: Get all feature files in the feature directory in sorted order  
-        self.fbank_files = sorted([os.path.join(self.fbank_dir, fname) for fname in os.listdir(self.fbank_dir)])
+        self.fbank_files = sorted(os.listdir(self.fbank_dir))
         
         # TODO: Take subset
         subset_size      = int(config['subset']*len(self.fbank_files))
@@ -107,7 +107,7 @@ class ASRDataset(Dataset):
             self.text_dir   = os.path.join(self.config['root'], self.partition, 'text')
 
             # TODO: Get all text files in the text directory in sorted order  
-            self.text_files = sorted([os.path.join(self.text_dir, fname) for fname in os.listdir(self.text_dir)])
+            self.text_files = sorted(os.listdir(self.text_dir))
             
             # TODO: Take subset
             self.text_files = self.text_files[:self.length]
@@ -142,7 +142,7 @@ class ASRDataset(Dataset):
         for i in tqdm(range(self.length)):
             # TODO: Load features
             # Features are of shape (num_feats, time)
-            feat = np.load(self.fbank_files[i])
+            feat = np.load(os.path.join(self.fbank_dir, self.fbank_files[i]))
             # TODO: Truncate features to num_feats set by you in the config
             feat = feat[:self.config['num_feats'], :]
 
@@ -169,7 +169,7 @@ class ASRDataset(Dataset):
             if self.partition != "test-clean":
                 # TODO: Load the transcript
                 # Note: Use np.load to load the numpy array and convert to list and then join to string 
-                transcript = np.load(self.text_files[i])
+                transcript = np.load(os.path.join(self.text_dir, self.text_files[i]))
                 transcript = ''.join(str(t) for t in transcript)
 
                 # TODO: Track character count (before tokenization)
